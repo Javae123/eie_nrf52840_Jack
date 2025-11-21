0dd9d69a3601df6e64914ef741945caf078b3698
Javae123 <esjackliu@gmail.com>
2025-11-20 18:42:39 -0700

    commit

--- app/src/main.c
+++ app/src/main.c
@@ -20,8 +20,19 @@ int main(void) {
   if (0 > LED_init()) {
     return 0;
   }
+  uint8_t current_duty_cycle = 0;
+  LED_pwm(LED0, current_duty_cycle);
 
   while(1) {
+    if(BTN_check_clear_pressed(BTN0)){
+      current_duty_cycle = (current_duty_cycle >= 100) ? 0 : (current_duty_cycle + 10);
+      printk("setting LED0 to %d%% brightness.\n", current_duty_cycle);
+      LED_pwm(LED0, current_duty_cycle);
+    }
+    if (BTN_check_clear_pressed(BTN1)) {
+    current_duty_cycle = (current_duty_cycle <= 0) ? 0 : (current_duty_cycle - 10);
+    LED_pwm(LED0, current_duty_cycle);
+    }
     k_msleep(SLEEP_MS);
   }
 	return 0;
